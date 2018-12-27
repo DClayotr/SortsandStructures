443aeddae264994e5241ecaa57aaa43c6fff72cc
Dyaln K Claytor <32943321+DClayotr@users.noreply.github.com>
2018-12-27 06:56:45 -0500

    Update Launcher.java

--- DoubleLinkedList/Launcher.java
+++ DoubleLinkedList/Launcher.java
@@ -2,7 +2,7 @@ package DoubleLinkedList;
 
 public class Launcher {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) { // test to make sure each of the functions work as intended
 		DoubleLinkedList list = new DoubleLinkedList();
 		list.add(7);
 		list.add(9);
